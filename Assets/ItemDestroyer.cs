1a77c6aa8f01848794e459422a6e18d99f9d4704
fugamiyata <72694228+fugamiyata@users.noreply.github.com>
2020-12-24 22:34:53 +0900

    Initial commit

---
+++ Assets/ItemDestroyer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroyer : MonoBehaviour
+{
+    
+
+    private GameObject mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+
+        this.mainCamera = GameObject.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //destroy
+        if (this.mainCamera.transform.position.z > this.transform.position.z)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/MyCameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MyCameraController : MonoBehaviour
+{
+
+    //Unityちゃんのオブジェクト
+    private GameObject unitychan;
+
+    //Unityちゃんとカメラの距離
+    private float difference;
+
+    
+
+
+    
+    void Start()
+    {
+
+        //Unityちゃんのオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
+
+        //Unityちゃんとカメラの距離
+        this.difference = unitychan.transform.position.z - this.transform.position.z;
+
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Unityちゃんの位置に合わせてカメラの位置を移動
+        this.transform.position = new Vector3(0, this.transform.position.y, this.unitychan.transform.position.z - difference);
+
+       
+    }
+}
